17396e7169ea4ea803ee3b19765fdd48edd42e64
sabari <muthusabari2710@gmail.com>
2025-07-30 12:24:32 +0530

    Assignment4 interface

---
+++ C2TC_CoreJavaPrograms/src/assignment4/Main.java
@@ -0,0 +1,13 @@
+package assignment4;
+
+public class Main {
+    public static void main(String[] args) {
+        AirIndia ai = new AirIndia(3, 1500.0);
+        ai.display();
+
+        Indigo indigo = new Indigo(2, 2000.0);
+        indigo.display();
+
+
+    }
+}
